fa443318c331c7519758a28558c2139f71a5597e
DanialD99 <danial781999@gmail.com>
2020-06-26 23:29:00 +0430

    main page html css and routing

---
+++ controllers/home/main.ts
@@ -0,0 +1,3 @@
+export function getMainView(ctx: any) {
+  ctx.render("./views/home/index.html");
+}
--- routes.ts
+++ routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
+import { getMainView } from "./controllers/home/main.ts";
 
 const router = new Router();
+
+router.get("/", getMainView);
 
 export default router;
