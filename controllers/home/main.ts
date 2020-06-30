d67939cd302abbf936b7e9e91d9c180a436d3e44
DanialD99 <danial781999@gmail.com>
2020-06-30 10:42:01 +0430

    getting data from database

--- controllers/home/main.ts
+++ controllers/home/main.ts
@@ -1,3 +1,12 @@
-export function getMainView(ctx: any) {
-  ctx.render("./views/home/index.html");
+import client from "../../models/config.ts";
+
+export async function getMainView(ctx: any) {
+  await client.connect();
+  const todos = await client.query(
+    "SELECT * FROM public.todos ORDER BY id ASC",
+  );
+  const context = {
+    "todos": todos.rowsOfObjects(),
+  };
+  ctx.render("./views/home/index.html", context);
 }
