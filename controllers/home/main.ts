41a550f366a0b6fa62f846dda2f0392e7e6db2bd
DanialD99 <danial781999@gmail.com>
2020-07-01 22:17:35 +0430

    new controller for adding todo

--- controllers/home/main.ts
+++ controllers/home/main.ts
@@ -3,10 +3,35 @@ import client from "../../models/config.ts";
 export async function getMainView(ctx: any) {
   await client.connect();
   const todos = await client.query(
-    "SELECT * FROM public.todos ORDER BY id ASC",
+    "SELECT * FROM public.todos ORDER BY id ASC"
   );
   const context = {
-    "todos": todos.rowsOfObjects(),
+    todos: todos.rowsOfObjects(),
   };
   ctx.render("./views/home/index.html", context);
+}
+
+export async function addTodo(ctx: any) {
+  const body = await ctx.request.body();
+  const newTodo = body.value.get("title");
+
+  if (newTodo != "") {
+    const add = await client.query(
+      "INSERT INTO todos(title, is_completed) VALUES($1, $2)",
+      newTodo,
+      false
+    );
+    const result = await client.query("SELECT * FROM todos ORDER BY id ASC");
+    ctx.response.status = 201;
+    ctx.response.body = {
+      success: true,
+      newList: result.rowsOfObjects(),
+    };
+  } else {
+    ctx.response.status = 400;
+    ctx.response.body = {
+      success: false,
+      message: "Title should be provided",
+    };
+  }
 }
